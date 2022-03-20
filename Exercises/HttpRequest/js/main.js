60be3adc92f79d2bf645243ecc6cdc3cd891f4ec
Guilherme Almeida <guimathiasgui@gmail.com>
2022-03-19 22:39:07 -0300

    Studying fetch API

--- Exercises/HttpRequest/js/main.js
+++ Exercises/HttpRequest/js/main.js
@@ -1,19 +1,3 @@
-const request = obj => {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true);
-    xhr.send();
-
-    xhr.addEventListener('load', () => {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        resolve(xhr.responseText);
-      } else {
-        reject(xhr.statusText);
-      }
-    });
-  });
-};
-
 document.addEventListener('click', e => {
   const el = e.target;
   const tag = el.tagName.toLowerCase();
@@ -25,15 +9,14 @@ document.addEventListener('click', e => {
 });
 
 async function loadPage(el) {
-  const href = el.getAttribute('href');
-  const objConfig = {
-    method: 'GET',
-    url: href,
-  };
-
   try {
-    const response = await request(objConfig);
-    loadResult(response);
+    const href = el.getAttribute('href');
+    const response = await fetch(href);
+
+    if (response.status !== 200) throw new Error('ERROR 404');
+
+    const html = await response.text();
+    loadResult(html);
   } catch (error) {
     console.log(error);
   }
