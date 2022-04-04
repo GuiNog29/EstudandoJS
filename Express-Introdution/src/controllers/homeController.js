92f7dccf0b73c8fd88c390067f2c78b089d9475a
Guilherme Almeida <guimathiasgui@gmail.com>
2022-04-04 20:56:59 -0300

    Injecting content into views

--- Express-Introdution/src/controllers/homeController.js
+++ Express-Introdution/src/controllers/homeController.js
@@ -1,7 +1,12 @@
 exports.initialPage = (req, res) => {
-  res.render('index');
+  res.render('index', {
+    title: 'Title Page',
+    numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  });
+  return;
 };
 
 exports.treatPost = (req, res) => {
-  res.send('aoba');
+  res.send(req.body);
+  return;
 };
