2bdb6300de7b381c5ab436ddf6335136efdf0fa7
Guilherme Almeida <guimathiasgui@gmail.com>
2022-04-03 22:34:34 -0300

    Applying Express Session

--- Express-Introdution/server.js
+++ Express-Introdution/server.js
@@ -11,12 +11,30 @@ mongoose
   })
   .catch(e => console.log(e));
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flashMessages = require('connect-flash');
+
 const routes = require('./routes');
 const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+  secret: '51612354651654948949441564165162635185',
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+  },
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING})
+});
+
+app.use(sessionOptions);
+app.use(flashMessages());
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
